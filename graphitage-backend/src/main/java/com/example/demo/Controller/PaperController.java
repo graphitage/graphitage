5dd5cd52d927a651ffc9baa446d386f8718dbf73
Akif Sönmez <akifsonmez@outlook.com>
2020-07-23 14:33:02 +0300

    Database connection changed.

--- graphitage-backend/src/main/java/com/example/demo/Controller/PaperController.java
+++ graphitage-backend/src/main/java/com/example/demo/Controller/PaperController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("http://localhost:3000/")
 @RestController
 @RequestMapping("/api.graphitage.com/papers")
 public class PaperController {
