40896a080695395806b668c3c9c7b5dd4cd85165
Akif Sönmez <akifsonmez@outlook.com>
2020-07-26 16:47:03 +0300

    CrossOrigin is added to DatasetController, LibraryController, PreprocessingController and ReaderController

--- graphitage-backend/src/main/java/com/example/demo/Controller/ReaderController.java
+++ graphitage-backend/src/main/java/com/example/demo/Controller/ReaderController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api.graphitage.com/readers")
 public class ReaderController {
