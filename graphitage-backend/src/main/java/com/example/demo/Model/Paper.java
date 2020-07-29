eac4690de27f5ad1bd0aafc25e2a26290ccaa8ad
onuroztnc <oztunconur@gmail.com>
2020-07-29 21:17:24 +0300

    Fixed updatePaper and addPaper methods.

--- graphitage-backend/src/main/java/com/example/demo/Model/Paper.java
+++ graphitage-backend/src/main/java/com/example/demo/Model/Paper.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.neo4j.ogm.annotation.*;
 
+import java.lang.reflect.Method;
+import java.security.cert.PKIXParameters;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -84,18 +86,18 @@ public class Paper extends PaperDetails implements Comparable<Paper> {
     }
 
     public Paper(String paperId, String paperIdType, String authors, List<String> keywords,
-                 String title,String abstractOfPaper, String linkOfPaper, Date publishDate){//semantic icin
+                 String title, String abstractOfPaper, String linkOfPaper, Date publishDate) {//semantic icin
         super.setAbstractOfPaper(abstractOfPaper);
         this.paperId = paperId;
         this.paperIdType = paperIdType;
         this.title = title;
         this.keywords = keywords;
         this.authors = authors;
-        this.linkOfPaper=linkOfPaper;
+        this.linkOfPaper = linkOfPaper;
         this.publishDate = publishDate;
     }
 
-    public Paper(String paperId, String paperIdType, String title){//semantic referans icin
+    public Paper(String paperId, String paperIdType, String title) {//semantic referans icin
         this.paperId = paperId;
         this.paperIdType = paperIdType;
         this.title = title;
@@ -117,5 +119,49 @@ public class Paper extends PaperDetails implements Comparable<Paper> {
     @Override
     public int hashCode() {
         return Objects.hash(paperId);
+    }
+
+    /*
+    * The non-null properties of the updatePaper are updated on the old paper.
+    */
+    public void merge(Paper updatePaper, List<String> withoutProperties) {
+
+        Method methods[] = Paper.class.getMethods();
+        /* All methods of the class have been traversed. */
+        for (Method method : methods) {
+
+            if (method.getName().startsWith("get")) {
+
+                boolean isContinue = true;
+                for (String withoutProperty : withoutProperties) {
+                    if (method.getName().contains(withoutProperty)) {
+                        isContinue = false;
+                    }
+                }
+                /* Setter and getter functions of property not included in the withoutProperty list were determined. */
+                if (isContinue) {
+                    String getterMethodName = method.getName();
+                    String setterMethodName = getterMethodName.replaceFirst("get", "set");
+
+                    try {
+                        /* The setter and receiver functions of the feature have been gotten. */
+                        Method getterMethod = Paper.class.getMethod(getterMethodName, (Class<?>) null);
+                        Method setterMethod = Paper.class.getMethod(setterMethodName, method.getReturnType());
+
+                        /* If the feature to be updated is not null, the updating was performed with the setter function. */
+                        Object value = getterMethod.invoke(updatePaper, (Object[]) null);
+                        if (value != null) {
+                            setterMethod.invoke(this, value);
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+        }
+
     }
 }
